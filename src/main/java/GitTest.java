a01b43f4be819838b995a0efe8ca314b5ab49efb
aman <872487443@qq.com>
2018-09-12 20:49:24 +0800

    branch1写了一个新方法

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,4 +4,17 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hi git!");
     }
+
+
+    /*b1写了一方法*/
+    public String test3(){
+
+        return null;
+    }
+
+
+
+
+
+
 }
